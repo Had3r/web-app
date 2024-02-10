af4930b5667901ec5e55ec593c7286ceb56dd698
Adrian Zawadzki <adrian.zawadzki@naturaily.com>
2024-02-10 17:55:24 +0100

    test-commit

--- frontend/src/components/AccountsTable/AccountsTable.tsx
+++ frontend/src/components/AccountsTable/AccountsTable.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
 
-export const AccountsTable = () => (
-  <div>
-    <h1>Accounts</h1>
-    <Link to="/">Back to Dashboard</Link>
-  </div>
-)
+export const AccountsTable = () => {
+  return (
+    <div>
+      <h1>Accounts</h1>
+      <Link to="/">Back to Dashboard</Link>
+    </div>
+  )
+}
