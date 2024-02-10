5c90f75765469333b7866860cf35e0a8cd804065
Adrian Zawadzki <adrian.zawadzki@naturaily.com>
2024-02-10 19:27:37 +0100

    refactor: cleaning redundant stuff

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,9 @@
 import { Link } from 'react-router-dom'
 
-export const Dashboard = () => {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <Link to="/create-account">Create Account</Link>
-      <Link to="/view-accounts">View Accounts</Link>
-    </div>
-  )
-}
+export const Dashboard = () => (
+  <div>
+    <h1>Dashboard</h1>
+    <Link to="/create-account">Create Account</Link>
+    <Link to="/view-accounts">View Accounts</Link>
+  </div>
+)
